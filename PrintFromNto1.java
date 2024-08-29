4dd1b070a6dd874ef99b178a18c0de6360b4f025
Abhishek Kaplesh <abkaplesh02@gmail.com>
2024-08-30 01:46:30 +0530

    updating files

---
+++ PrintFromNto1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class PrintFromNto1 {
+    public static void Print(int n){
+        if(n==0){
+            return;
+        }
+        System.out.println(n);
+        Print(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of n :: ");
+        int a=sc.nextInt();
+        Print(a);
+    }
+}
