115a071fc6c8efe870fedeb56f585f435dc50c55
Abhishek Kaplesh <abkaplesh02@gmail.com>
2024-08-30 01:45:49 +0530

    adding files

---
+++ PrintFrom1toN.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class PrintFrom1toN {
+    public static void Print(int a, int n){
+        if(a>n){
+            return ;
+        }
+        System.out.println(a);
+        Print(a+1,n);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of n :: ");
+        int a=sc.nextInt();
+        Print(1,a);
+
+    }
+}
