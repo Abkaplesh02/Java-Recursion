fefcc786199eb8283edf653797b14e4dffcecd4f
Abhishek Kaplesh <abkaplesh02@gmail.com>
2024-08-31 07:30:08 +0530

    adding files

---
+++ CheckIfStringisPalindromeUsingRecursion.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class CheckIfStringisPalindromeUsingRecursion {
+    public static boolean Reverse(String str, int n){
+        if(n>(str.length()-1)/2){
+            return true;
+        }
+        if(str.charAt(n)!=str.charAt(str.length()-1-n)){
+            return false;
+        }
+        return Reverse(str,n+1);
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the string to check for plaindrome :: ");
+        String str=sc.next();
+        System.out.println("is Given string palindrome :: " + Reverse(str, 0));
+    }
+}
