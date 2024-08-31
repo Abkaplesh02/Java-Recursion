8f4e30003c78639392d8b16e092f65ddcf8ca2ff
Abhishek Kaplesh <abkaplesh02@gmail.com>
2024-08-31 07:31:14 +0530

    adding files

---
+++ PrintNto1usingbacktrackingrecursion.java
@@ -0,0 +1,21 @@
+import java.nio.channels.Pipe;
+import java.util.Scanner;
+
+public class PrintNto1usingbacktrackingrecursion {
+    public static void Print(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        else{
+            System.out.println(n);
+            Print(n-1);
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the value of n :: ");
+        int a=sc.nextInt();
+        Print(a);
+    }
+}
